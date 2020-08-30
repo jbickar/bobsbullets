a4001816e6fe2fa7ce60e92f7db44bc7ee19ea7f
John Bickar <jbickar@stanford.edu>
2020-08-30 09:31:58 -0700

    Initial commit of static site

--- scripts/refresh-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "Are you sure you want to completely blow away the database and files on the local bob.dev and replace with database and files from bobsbullets.net? [Y/N]: " destroy
-if test $destroy = Y; then
-  drush @bob.local -y sql-drop
-  drush -y sql-sync @bob.prod @bob.local
-  drush -y rsync @bob.prod:%files @bob.local:%files
-  drush @bob.local cr
-fi
